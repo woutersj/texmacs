woutersj/texmacs

commit fe589db612e8eab870f394059d77cc31057b857d

Better error messages handling in Scheme sessions.
git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@5884 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/dynamic/session-edit.scm
@@ -89,19 +89,29 @@
 
 (define (eval-string-with-catch s)
   (catch #t
-	 (lambda () (eval (string->object s)))
-	 (lambda (key msg . args)
-	   key)))
+         (lambda () (eval (string->object s)))
+         (lambda (key msg . err-msg)
+           (let* ((msg (car err-msg))
+                  (args (cdr err-msg))
+                  (args (sublist args 0 (string-count-occurrences "~" msg)))
+                  (args (map car args))
+                  (err-msg (eval (apply format #f msg args))))
+             (stree->tree `(errput ,err-msg))))))
+
+(define (error-tree? t)
+  (and (tree? t) (tree-is? t 'errput)))
 
 (tm-define (scheme-eval t)
   (let* ((s (texmacs->code t))
 	 (r (eval-string-with-catch s)))
-    (cond ((and (tree? r) (session-scheme-trees?))
+    (cond ((and (tree? r) (error-tree? r) (session-scheme-trees?))
+           (tree-copy r))
+          ((and (tree? r) (session-scheme-trees?))
            (tree 'text (tree-copy r)))
-	  ((session-scheme-math?)
-	   (with m (cas->stree r)
-	     (if (tm? m) (tree 'math (tm->tree m)) (var-object->string r))))
-	  (else (var-object->string r)))))
+          ((session-scheme-math?)
+           (with m (cas->stree r)
+                 (if (tm? m) (tree 'math (tm->tree m)) (var-object->string r))))
+          (else (var-object->string r)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Low-level evaluation management

src/Scheme/Glue/build-glue-basic.scm
@@ -212,6 +212,7 @@
   ;; routines for strings
   (string-number? is_double (bool string))
   (string-occurs? occurs (bool string string))
+  (string-count-occurrences count_occurrences (int string string))
   (string-search-forwards search_forwards (int string int string))
   (string-search-backwards search_backwards (int string int string))
   (string-replace replace (string string string string))

src/Scheme/Glue/glue_basic.cpp
@@ -2352,6 +2352,21 @@ tmg_string_occursP (tmscm arg1, tmscm arg2) {
 }
 
 tmscm
+tmg_string_count_occurrences (tmscm arg1, tmscm arg2) {
+  TMSCM_ASSERT_STRING (arg1, TMSCM_ARG1, "string-count-occurrences");
+  TMSCM_ASSERT_STRING (arg2, TMSCM_ARG2, "string-count-occurrences");
+
+  string in1= tmscm_to_string (arg1);
+  string in2= tmscm_to_string (arg2);
+
+  // TMSCM_DEFER_INTS;
+  int out= count_occurrences (in1, in2);
+  // TMSCM_ALLOW_INTS;
+
+  return int_to_tmscm (out);
+}
+
+tmscm
 tmg_string_search_forwards (tmscm arg1, tmscm arg2, tmscm arg3) {
   TMSCM_ASSERT_STRING (arg1, TMSCM_ARG1, "string-search-forwards");
   TMSCM_ASSERT_INT (arg2, TMSCM_ARG2, "string-search-forwards");
@@ -6823,6 +6838,7 @@ initialize_glue_basic () {
   tmscm_install_procedure ("graphics-notify-update",  tmg_graphics_notify_update, 1, 0, 0);
   tmscm_install_procedure ("string-number?",  tmg_string_numberP, 1, 0, 0);
   tmscm_install_procedure ("string-occurs?",  tmg_string_occursP, 2, 0, 0);
+  tmscm_install_procedure ("string-count-occurrences",  tmg_string_count_occurrences, 2, 0, 0);
   tmscm_install_procedure ("string-search-forwards",  tmg_string_search_forwards, 3, 0, 0);
   tmscm_install_procedure ("string-search-backwards",  tmg_string_search_backwards, 3, 0, 0);
   tmscm_install_procedure ("string-replace",  tmg_string_replace, 3, 0, 0);

commit 0449e31841e2e80eabd9b98356eb5562c970ea77

Fix bug introduced by r5884
git-svn-id: svn+ssh://svn.savannah.gnu.org/texmacs/trunk/src@5885 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/dynamic/session-edit.scm
@@ -92,10 +92,9 @@
          (lambda () (eval (string->object s)))
          (lambda (key msg . err-msg)
            (let* ((msg (car err-msg))
-                  (args (cdr err-msg))
-                  (args (sublist args 0 (string-count-occurrences "~" msg)))
-                  (args (map car args))
-                  (err-msg (eval (apply format #f msg args))))
+                  (args (cadr err-msg))
+                  (err-msg 
+                    (if (list? args) (eval (apply format #f msg args)) msg)))
              (stree->tree `(errput ,err-msg))))))
 
 (define (error-tree? t)